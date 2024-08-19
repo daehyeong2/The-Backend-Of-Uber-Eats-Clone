ea6431fa03ba460d341fc2fb861b987057691fa1
daehyeong2 <baconbacon1231@gmail.com>
2024-08-19 09:28:34 +0900

    Validating ArgsTypes, TypeORM Setup (2.5~3.3)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '172.23.112.1',
+      port: 5432,
+      username: 'gorani',
+      password: '123456',
+      database: 'nuber-eats',
+      synchronize: true,
+      logging: true,
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
