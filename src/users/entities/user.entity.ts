2dcc19db7c2a792031d90c920955b9de8123ff5b
daehyeong2 <baconbacon1231@gmail.com>
2024-08-21 02:41:56 +0900

    Create Account Mutation part One (5.3)

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,14 +1,26 @@
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
 import { CoreEntity } from '../../common/entities/core.entity';
 import { Column, Entity } from 'typeorm';
+import { IsEmail, IsString } from 'class-validator';
 
 type UserRole = 'client' | 'owner' | 'delivery';
 
+@InputType({ isAbstract: true })
+@ObjectType()
 @Entity()
 export class User extends CoreEntity {
+  @Field(type => String)
   @Column()
+  @IsEmail()
   email: string;
+
+  @Field(type => String)
   @Column()
+  @IsString()
   password: string;
+
+  @Field(type => String)
   @Column()
+  @IsString()
   role: UserRole;
 }
