edee15c2f019b991791382ed7f1db91d2deedb3d
daehyeong2 <baconbacon1231@gmail.com>
2024-08-22 13:40:46 +0900

    JWT Module part Three (6.5)

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,15 +4,12 @@ import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { CreateAccountInput } from './dtos/create-account.dto';
 import { LoginInput } from './dtos/login.dto';
-import * as jwt from 'jsonwebtoken';
-import { ConfigService } from '@nestjs/config';
 import { JwtService } from '../jwt/jwt.service';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private readonly users: Repository<User>,
-    private readonly config: ConfigService,
     private readonly jwtService: JwtService,
   ) {}
   async createAccount({
@@ -51,7 +48,7 @@ export class UsersService {
           error: '비밀번호가 일치하지 않습니다.',
         };
       }
-      const token = jwt.sign({ id: user.id }, this.config.get('PRIVATE_KEY'));
+      const token = this.jwtService.sign(user.id + '');
       return {
         ok: true,
         token,
