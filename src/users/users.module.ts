aa5ac5f65a49d0227e9bfd96326baf53bab48f54
daehyeong2 <baconbacon1231@gmail.com>
2024-08-22 10:45:19 +0900

    Introduction to Authentication (6.0)

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), ConfigModule],
   providers: [UsersResolver, UsersService],
 })
 export class UsersModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,11 +4,14 @@ import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { CreateAccountInput } from './dtos/create-account.dto';
 import { LoginInput } from './dtos/login.dto';
+import * as jwt from 'jsonwebtoken';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private readonly users: Repository<User>,
+    private readonly config: ConfigService,
   ) {}
   async createAccount({
     email,
@@ -46,6 +49,7 @@ export class UsersService {
           error: '비밀번호가 일치하지 않습니다.',
         };
       }
+      const token = jwt.sign({ id: user.id }, this.config.get('SECRET_KEY'));
       return {
         ok: true,
         token: 'lalalala',
