9ffba98ad5375a5fa079d0a9bddd9168121b9282
daehyeong2 <baconbacon1231@gmail.com>
2024-08-29 02:49:01 +0900

    JWT Sign Test (9.1)

--- src/jwt/jwt.service.spec.ts
+++ src/jwt/jwt.service.spec.ts
@@ -1,6 +1,13 @@
 import { Test } from '@nestjs/testing';
 import { JwtService } from './jwt.service';
+import * as jwt from 'jsonwebtoken';
 import { CONFIG_OPTIONS } from '@app/common/common.constants';
+
+jest.mock('jsonwebtoken', () => {
+  return {
+    sign: jest.fn(() => 'TOKEN'),
+  };
+});
 
 const TEST_KEY = 'testKey';
 
@@ -21,7 +28,17 @@ describe('JwtService', () => {
     expect(service).toBeDefined();
   });
 
-  it.todo('sign');
+  describe('sign', () => {
+    it('should return a signed token', () => {
+      const ID = '1';
+      const token = service.sign(ID);
+      expect(typeof token).toBe('string');
+      expect(jwt.sign).toHaveBeenCalledTimes(1);
+      expect(jwt.sign).toHaveBeenCalledWith({ id: ID }, TEST_KEY);
+    });
+  });
 
-  it.todo('verify');
+  describe('verify', () => {
+    it('should return the decoded token', () => {});
+  });
 });
