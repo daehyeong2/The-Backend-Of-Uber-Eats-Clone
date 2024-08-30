2d2cd842750351171bf2320794f2a828e7851e66
daehyeong2 <baconbacon1231@gmail.com>
2024-08-31 06:19:29 +0900

    Testing createAccount part One (10.2)

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -2,6 +2,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import { AppModule } from '../src/app.module';
 import { getConnection } from 'typeorm';
+import * as request from 'supertest';
+
+const GRAPHQL_ENDPOINT = '/graphql';
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
@@ -20,10 +23,35 @@ describe('UserModule (e2e)', () => {
     app.close();
   });
 
-  it.todo('me');
+  describe('createAccount', () => {
+    const EMAIL = 'baconbacon1231@gmail.com';
+    it('should create account', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `mutation {
+            createAccount(input: {
+              email: "${EMAIL}",
+              password: "1234",
+              role: Client
+            }) {
+              ok
+              error
+            }
+          }`,
+        })
+        .expect(200)
+        .expect(res => {
+          expect(res.body.data.createAccount.ok).toBeTruthy();
+          expect(res.body.data.createAccount.error).toBeNull();
+        });
+    });
+
+    it.todo('should fail if account already exists');
+  });
   it.todo('userProfile');
-  it.todo('createAccount');
   it.todo('login');
-  it.todo('editProfile');
+  it.todo('me');
   it.todo('verifyEmail');
+  it.todo('editProfile');
 });
