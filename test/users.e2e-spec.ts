ad3cfe73c7d0215df48fab2784977778d8c33394
daehyeong2 <baconbacon1231@gmail.com>
2024-09-01 23:52:50 +0900

    Testing me (10.6)

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -211,18 +211,67 @@ describe('UserModule (e2e)', () => {
         .expect(res => {
           const {
             body: {
-              data: { ok, error, user },
+              data: {
+                userProfile: { ok, error, user },
+              },
             },
           } = res;
-
           expect(ok).toBeFalsy();
+          console.log(error);
           expect(error).toEqual(expect.any(String));
           expect(user).toBeNull();
         });
     });
   });
 
-  it.todo('me');
+  describe('me', () => {
+    it('should find my profile', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .set('X-JWT', jwtToken)
+        .send({
+          query: `
+          {
+            me{
+              email
+            }
+          }
+        `,
+        })
+        .expect(200)
+        .expect(res => {
+          const {
+            body: {
+              data: {
+                me: { email },
+              },
+            },
+          } = res;
+          expect(email).toBe(testUser.email);
+        });
+    });
+
+    it('should not allow logged out user', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          {
+            me{
+              email
+            }
+          }
+        `,
+        })
+        .expect(200)
+        .expect(res => {
+          const {
+            body: { errors },
+          } = res;
+          expect(errors[0].message).toBe('Forbidden resource');
+        });
+    });
+  });
   it.todo('verifyEmail');
   it.todo('editProfile');
 });
