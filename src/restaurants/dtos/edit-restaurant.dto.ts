9d1b014154cdfe39a9bb5af61cbf38a9677aaf3c
daehyeong2 <baconbacon1231@gmail.com>
2024-09-03 17:31:03 +0900

    Edit Restaurant Testing (11.10)

--- src/restaurants/dtos/edit-restaurant.dto.ts
+++ src/restaurants/dtos/edit-restaurant.dto.ts
@@ -1,12 +1,12 @@
 import { Field, InputType, ObjectType, PartialType } from '@nestjs/graphql';
 import { CreateRestaurantInput } from './create-restaurant.dto';
 import { CoreOutput } from '@app/common/dtos/output.dto';
-import { IsString } from 'class-validator';
+import { IsNumber } from 'class-validator';
 
 @InputType()
 export class EditRestaurantInput extends PartialType(CreateRestaurantInput) {
   @Field(type => Number)
-  @IsString()
+  @IsNumber()
   restaurantId: number;
 }
 
