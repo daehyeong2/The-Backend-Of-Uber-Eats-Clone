6eb495d21573fbcd0c4afa862145fa2ef551ec62
daehyeong2 <baconbacon1231@gmail.com>
2024-09-15 08:58:02 +0900

    File Upload part Two (22.3)

--- src/app.module.ts
+++ src/app.module.ts
@@ -42,6 +42,9 @@ import { UploadsModule } from './uploads/uploads.module';
         MAILGUN_API_KEY: Joi.string().required(),
         MAILGUN_DOMAIN_NAME: Joi.string().required(),
         MAILGUN_FROM_EMAIL: Joi.string().required(),
+        AWS_ACCESS_KEY: Joi.string().required(),
+        AWS_SECRET_ACCESS_KEY: Joi.string().required(),
+        BUCKET_NAME: Joi.string().required(),
       }),
     }),
     TypeOrmModule.forRoot({
--- src/uploads/uploads.controller.ts
+++ src/uploads/uploads.controller.ts
@@ -4,13 +4,40 @@ import {
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { FileInterceptor } from '@nestjs/platform-express';
+import * as AWS from 'aws-sdk';
 
 @Controller('uploads')
 export class UploadsController {
+  constructor(private readonly configService: ConfigService) {}
   @Post()
   @UseInterceptors(FileInterceptor('file'))
-  uploadFile(@UploadedFile() file) {
-    console.log(file);
+  async uploadFile(@UploadedFile() file) {
+    AWS.config.update({
+      region: 'ap-northeast-2',
+      credentials: {
+        accessKeyId: this.configService.get('AWS_ACCESS_KEY'),
+        secretAccessKey: this.configService.get('AWS_SECRET_ACCESS_KEY'),
+      },
+    });
+    try {
+      const objectName = `${Date.now()
+        .toString()
+        .substring(5)}-${file.originalname}`;
+      await new AWS.S3()
+        .putObject({
+          Body: file.buffer,
+          Bucket: this.configService.get('BUCKET_NAME'),
+          Key: objectName,
+          ACL: 'public-read',
+        })
+        .promise();
+      const url = `https://guber-eats.s3.ap-northeast-2.amazonaws.com/${objectName}`;
+      return { url };
+    } catch (e) {
+      console.log(e);
+      return null;
+    }
   }
 }
